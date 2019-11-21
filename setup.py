a7f45e3d1ffec6bcf27b5e7e20c99bbde3cad407
Alexandre <alexandre.angleraud@gmail.com>
2019-11-21 16:56:15 +0200

    Fix version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='jena_sempy',
-    version='0.6',
+    version='0.6.1',
     description='Handle communication between python and Jena triple store',
     author='Alexandre Angleraud',
     author_email='alexandre.angleraud@tuni.fi',
-    download_url='https://github.com/Zorrander/jena_sempy/archive/v_0.6.tar.gz',
+    download_url='https://github.com/Zorrander/jena_sempy/archive/v_0.6.1.tar.gz',
     license='New BSD License',
     test_suite="tests",
     install_requires=[
