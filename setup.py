159be9cc942bed36ffab2f5b139528a195e729e1
Alexandre <alexandre.angleraud@gmail.com>
2019-12-01 02:10:16 +0200

    Fix I/O error

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='jena_sempy',
-    version='0.7.7',
+    version='0.7.8',
     description='Manipulates semantic information and offers planning capabilities.',
     author='Alexandre Angleraud',
     author_email='alexandre.angleraud@tuni.fi',
-    download_url='https://github.com/Zorrander/jena_sempy/archive/v_0.7.7.tar.gz',
+    download_url='https://github.com/Zorrander/jena_sempy/archive/v_0.7.8.tar.gz',
     license='New BSD License',
     test_suite="tests",
     install_requires=[
