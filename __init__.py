d6fa32d9672fcc96cbf2dd15402c3928e635b00e
Alexandre <alexandre.angleraud@gmail.com>
2019-12-01 04:38:50 +0200

    Export policy class

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from jena_com.communication import Server
 from jena_reasoning.owl import Knowledge
+from jena_models.policies import Policy
 from jena_models.base_solution import BaseSolution
 from jena_models.set_of_differences import SetOfDifferences
 from jena_models.update_rules import dynamic_backpropagation_rule_I, dynamic_backpropagation_rule_II
