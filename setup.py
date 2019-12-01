7ba5dd0c76bb232fd3a26b872f9294575a1bfbe0
Alexandre <alexandre.angleraud@gmail.com>
2019-12-01 04:29:04 +0200

    Version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='jena_sempy',
-    version='0.7.9',
+    version='0.8.0',
     description='Manipulates semantic information and offers planning capabilities.',
     author='Alexandre Angleraud',
     author_email='alexandre.angleraud@tuni.fi',
-    download_url='https://github.com/Zorrander/jena_sempy/archive/v_0.7.9.tar.gz',
+    download_url='https://github.com/Zorrander/jena_sempy/archive/v_0.8.0.tar.gz',
     license='New BSD License',
     test_suite="tests",
     install_requires=[
