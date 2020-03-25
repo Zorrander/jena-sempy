20dd0365c5b33629c03fce0adf015977c4bbd43b
Alexandre Angleraud <alexandre.angleraud@gmail.com>
2020-03-26 01:03:04 +0200

    fix naming individuals

--- jena_com/communication.py
+++ jena_com/communication.py
@@ -1,6 +1,5 @@
 from SPARQLWrapper import SPARQLWrapper, JSON, POST, TURTLE, XML
 from more_itertools import grouper
-from . import queries as qry
 
 '''
 modifiers= [
@@ -18,27 +17,44 @@ class FusekiServer:
         self.query_sparql_store = SPARQLWrapper("http://"+host+"/"+ dataset +"/sparql")  # serviceQuery
         self.update_sparql_store = SPARQLWrapper("http://"+host+"/"+ dataset +"/update")  # updateQuery
 
+    @staticmethod
+    def concatenate_prefix(query):
+        return """
+            PREFIX cogrob: <http://onto-server-tuni.herokuapp.com/Panda#>
+            PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX owl:  <http://www.w3.org/2002/07/owl#>
+            PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
+        """ + query
+
 
     def generate_instance_uri(self, class_name):
         query = """
             SELECT ?s
             WHERE {
-                ?s rdf:type cogrob:"""+class_name+"""
+                ?s rdf:type cogrob:"""+class_name+""" .
             }
         """
         instances = self.select_operation(query)
-        index = max([inst[-1] for inst in instances]) if instances else 0
-        return "cogrob:"+ class_name[0].lower() + class_name[1:]+"Ind"+str(index)
+        index = max([inst['s']['value'][-1] for inst in instances]) if instances else -1
+        index = str(int(index)+1)
+        return "cogrob:"+ class_name[0].lower() + class_name[1:]+"Ind"+index
 
     def create_instance(self, class_name):
-        uri = self.generate_instance_uri(class_name)
-        return self.add_data(uri, "rdf:type", class_name)
+        subject = self.generate_instance_uri(class_name)
+        predicate = "rdf:type"
+        object = "cogrob:" + class_name
+        query = """
+            INSERT DATA {
+                """ + subject + """ """ + predicate + """ """ + object + """
+            }"""
+        return self.update_operation(query)
 
     def select_operation(self, query):
         ''' Returns variables bound in a query pattern match '''
         try:
             self.query_sparql_store.setReturnFormat(JSON)
-            self.query_sparql_store.setQuery(query)
+            self.query_sparql_store.setQuery(self.concatenate_prefix(query))
             dict_result = self.query_sparql_store.query().convert()
             return [x for x in dict_result["results"]["bindings"]]
         except:
@@ -48,9 +64,10 @@ class FusekiServer:
         ''' DELETE or INSERT operations '''
         try:
             self.update_sparql_store.setMethod(POST)
-            self.update_sparql_store.setQuery(query)
-            self.update_sparql_store.query()
-            # return results.response.read()
+            self.update_sparql_store.setQuery(self.concatenate_prefix(query))
+            results = self.update_sparql_store.query()
+            #return results.response.read()
+            return True
         except:
             print("Could not complete UPDATE operation")
 
@@ -58,7 +75,7 @@ class FusekiServer:
         ''' Returns a boolean indicating whether a query pattern matches or not. '''
         try:
             self.query_sparql_store.setReturnFormat(JSON)
-            self.query_sparql_store.setQuery(query)
+            self.query_sparql_store.setQuery(self.concatenate_prefix(query))
             dict_result = self.query_sparql_store.query().convert()
             return dict_result['boolean']
         except Exception as e:
@@ -68,7 +85,7 @@ class FusekiServer:
         '''  Returns an RDF graph that describes the resources found. '''
         try:
             self.query_sparql_store.setReturnFormat(XML)
-            self.query_sparql_store.setQuery(query)
+            self.query_sparql_store.setQuery(self.concatenate_prefix(query))
             results = self.query_sparql_store.query().convert()
             str_result = results.serialize(format='nt')
             uris = str_result.split(' ')
